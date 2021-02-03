da5e65d8311c2c9f44337370481fd40694f2803c
celliniluong <78052138+celliniluong@users.noreply.github.com>
2021-02-02 16:26:56 -0800

    Update sketch.js
    Added 10 shapes

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
 
 // Setup code goes here
 function setup() {
-  createCanvas(1200, 1600);
+  createCanvas(800, 800);
+
  }
 
 
 // Draw code goes here
 function draw() {
-  background(200);
+  background(80);
+
+
+  fill(204, 101, 192, 127);
+  rect(35, 220, 220, 60);
+  ellipse(60, 60, 80, 80);
+  triangle(300, 200, 320, 200, 310, 180);
+  arc(120, 60, 80, 80, 0, HALF_PI);
+  line(500, 500, 800, 800);
+  quad(300, 400, 350, 450, 500, 550, 600, 650);
+  ellipse(500, 90, 60, 120);
+  quad(158, 40, 200, 30, 300, 50, 400, 100);
+  triangle(800, 600, 800, 300, 650, 500);
+  arc(390, 400, 80, 80, QUARTER_PI, PI+QUARTER_PI);
+  
+
 }
